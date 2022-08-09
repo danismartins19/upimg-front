452d036aca5232d36bdb2afd7c90522bf2055ef3
Daniel Martins <62864768+danismartins19@users.noreply.github.com>
2022-08-09 17:59:40 -0300

    upload adicionado

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,13 +1,39 @@
-
+import React, { useEffect, useState } from "react";
+import { FileUploader } from "react-drag-drop-files";
 
 function Home() {
-  
+  let base64string : any = "";
+  const [file,setFile] = useState<any>(null);
+  const fileTypes = ["JPG", "PNG"];
+  const handleDropFiles = (file : File) =>{
+    setFile(file)
+  }
+
+  useEffect(()=>{
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      base64string = reader.result;
+    }
+
+    console.log(base64string);
+    
+  },[file])
+
 
   return (
     <>
-    Olá Mundo
+     <FileUploader
+        multiple={true}
+        handleChange={handleDropFiles}
+        name="file"
+        label="Selecione ou arraste a imagem aqui"
+        hoverTitle="Solte aqui"
+        types={fileTypes}
+      />
+      <p>{file ? `${file[0].name} adicionado!` : "Nenhum arquivo adicionado!"}</p>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
