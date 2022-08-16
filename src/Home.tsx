4929b97844eae3603a6c159f714a69c4da8eeddd
Daniel Martins <danidiamoo1@gmail.com>
2022-08-15 21:08:41 -0300

    funcao de upload criada

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,24 +1,30 @@
 import React, { useEffect, useState } from "react";
 import { FileUploader } from "react-drag-drop-files";
+import axios from 'axios';
 
 function Home() {
   let base64string : any = "";
   const fileTypes = ["JPG", "PNG"];
+  const [fileName, setFileName] = useState<string>("");
 
   const handleDropFiles = (file : any) =>{
     var reader = new FileReader();
     if(file[0]){
       reader.readAsDataURL(file[0])
+      setFileName(file[0].name)
     }
 
     reader.onloadend = () =>{
       base64string = reader.result;
-      console.log(base64string)
     }
   }
 
+  const sendImage = async () =>{
+    let req = await axios.post(`${import.meta.BASE_URL}/`, {
+        img: base64string
+    })
 
-  
+  }
 
   return (
     <>
@@ -30,6 +36,18 @@ function Home() {
         hoverTitle="Solte aqui"
         types={fileTypes}
       />
+
+      { fileName != "" && 
+
+        <>
+          <h4>{fileName}</h4>
+          <br />
+          <button onClick={sendImage}>Enviar imagem</button>
+        </>
+
+
+      }
+
     </>
   );
 }
